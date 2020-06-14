a0160f718535bd7065042c06f0d6a7c0bbc5faf9
warnov <warnov@gmail.com>
2020-06-14 00:13:55 -0500

    Add project files.

---
+++ Minecraft.KeySender.Win/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Minecraft.KeySender.Win
+{
+    public partial class Form1 : Form
+    {
+        readonly string _commandTemplate = "/fill {0} {1} {2} {3} {4} {5} sandstone_stairs 3";
+        int x1 = 294, y1 = 63, z1 = 162, x2 = 344, y2 = 63, z2 = 162;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            Thread.Sleep(5000);
+
+            for (int i = 0; i < 25; i++)
+            {
+                x1++;
+                y1++;
+                z1--;
+                x2--;
+                y2++;
+                z2--;
+                var command = String.Format(_commandTemplate, x1, y1, z1, x2, y2, z2);                
+                SendKeys.Send("{ENTER}");
+                Thread.Sleep(2000);
+                SendKeys.Send(command);
+                Thread.Sleep(1800);
+                SendKeys.Send("{ENTER}");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ Minecraft.KeySender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Minecraft.KeySender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandTemplate = "/fill {0} {1} {2} {3} {4} {5} {6} sandstone_stairs 2";
+            Console.WriteLine("Hello World!");
+            //            344 63  112 294 63  112
+            //            343 64  113 295 64  113
+            int x1=343, y1=64, z1=113, x2=295, y2=64, z2=113;
+            for (int i = 0; i < 10; i++)
+            {
+                x1--;
+                y1++;
+                z1++;
+                x2--;
+                y2++;
+                z2++;
+                var command = String.Format(commandTemplate, x1, y1, z1, x2, y2, z2);
+
+                Thread.Sleep(5000);
+            }
+
+        }
+    }
+}
